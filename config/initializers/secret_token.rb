486c5252385beac332ab4b411a98126f50fa80f1
Mc Cheung <mc.cheung(at).aol.com>
2012-10-28 08:38:40 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreeRead::Application.config.secret_token = 'a110549faf30550fa48cadcfc696f2dae908edeac1b5e2632e41763c919421b31b4135c3480bedde70a831110b187b17e74020ddb402737b528748cebf335809'
